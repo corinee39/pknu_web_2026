15f15461ee4b07da669e69244931db2391380f6e
corinee39 <corinee39@gmail.com>
2026-04-03 09:32:38 +0900

    15일차 nodejs 수업 완료

--- nodejs/n03.js
+++ nodejs/n03.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 3013;
-
+const path = require("path");
 const _path = path.join(__dirname, "dist");
 
 app.use(express.static(_path)); // 정적 서비스로 index.html 파일 서빙
